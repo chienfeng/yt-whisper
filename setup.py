a960ea9b27e07a867a535607bda211226c39cdbc
chienfeng <35622371+chienfeng@users.noreply.github.com>
2023-04-09 00:02:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author="Miguel Piedrafita",
     install_requires=[
         'yt-dlp',
-        'whisper @ git+https://github.com/openai/whisper.git'
+        'openai-whisper @ git+https://github.com/openai/whisper.git'
     ],
     description="Generate subtitles for YouTube videos using Whisper",
     entry_points={
